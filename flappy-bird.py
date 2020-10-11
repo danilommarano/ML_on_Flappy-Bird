f8a3126f6720ff4ae2216f3d21c273be9197d89e
Danilo Matrangolo Marano <dpc@pop-os.localdomain>
2020-10-11 16:54:49 -0300

    Created the Bird class

--- flappy-bird.py
+++ flappy-bird.py
@@ -7,10 +7,37 @@ import time
 WIDTH = 600
 HEIGHT = 800
 
-# ─────────────────────────────── Load Images ─────────────────────────────── #
+# ──────────────────────────────── Load Images ─────────────────────────────── #
 BIRD_IMGS =[pg.transform.scale2x(pg.image.load(os.path.join("imgs", "bird1.png"))),
             pg.transform.scale2x(pg.image.load(os.path.join("imgs", "bird2.png"))),
             pg.transform.scale2x(pg.image.load(os.path.join("imgs", "bird3.png")))]
 PIPE_IMG = pg.transform.scale2x(pg.image.load(os.path.join("imgs", "pipe.png")))
 FLOOR_IMG = pg.transform.scale2x(pg.image.load(os.path.join("imgs", "base.png")))
 BG_IMG = pg.transform.scale2x(pg.image.load(os.path.join("imgs", "bg.png")))
+
+# ──────────────────────────── Object of the Bird ──────────────────────────── #
+class Bird:
+    IMGS = BIRD_IMGS
+    MAX_ROTATION = 25
+    ROT_VEL = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.vel = 0
+        self.height = self.y
+        self.img_count = 0
+        self.img = self.IMGS[0]
+
+    def jump(self):
+        self.vel = -10.5
+        self.tick_count = 0
+        self.height = self.y
+
+    def move(self):
+        self.tick_count += 1
+
+        d = self.vel * self.tick_count + 1.5 * self.tick_count ** 2
