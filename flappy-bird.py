5d5be200e96ed70af420195dfb97861d7dc11a92
Danilo Matrangolo Marano <dpc@pop-os.localdomain>
2020-10-11 17:57:32 -0300

    The game runs with the bird falling

--- flappy-bird.py
+++ flappy-bird.py
@@ -4,7 +4,7 @@ import pygame as pg
 import neat
 import time
 
-WIDTH = 600
+WIDTH = 550
 HEIGHT = 800
 
 # ──────────────────────────────── Load Images ─────────────────────────────── #
@@ -40,4 +40,73 @@ class Bird:
     def move(self):
         self.tick_count += 1
 
+        # ─ Physics for the movementation ─ #
         d = self.vel * self.tick_count + 1.5 * self.tick_count ** 2
+
+        if d >= 16:
+            d = d/abs(d) * 16
+
+        elif d < 0:
+            d -= 2
+
+        self.y = self.y + d
+
+        if d < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        else:
+            if self.tilt > -90:
+                self.tilt -= self.ROT_VEL
+
+    def draw(self, win):
+        self.img_count += 1
+
+        # ─ Set the correct bir image base on the animation time ─ #
+        if self.img_count <self.ANIMATION_TIME:
+            self.img = self.IMGS[0]
+        elif self.img_count < self.ANIMATION_TIME * 2:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME * 3:
+            self.img = self.IMGS[2]
+        elif self.img_count < self.ANIMATION_TIME * 4:
+            self.img = self.IMGS[1]
+        elif self.img_count == self.ANIMATION_TIME * 4 + 1:
+            self.img = self.IMGS[1]
+            self.img_count = 0
+        if self.tilt <= -80:
+            self.img = self.IMGS[1]
+            self.img_count = self.ANIMATION_TIME * 2
+
+        # ─ Rotate bird image ─ #
+        rotated_image = pg.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(
+            center=self.img.get_rect(topleft = (self.x, self.y)).center)
+        win.blit(rotated_image, new_rect.topleft)
+
+    def get_mask(self):
+        return pg.mask.form_surface(self.img)
+
+
+
+def draw_window(win,bird):
+    win.blit(BG_IMG, (0,0))
+    bird.draw(win)
+    pg.display.update()
+
+def main():
+    bird = Bird(200, 200)
+    run = True
+    win = pg.display.set_mode((WIDTH, HEIGHT))
+
+    while run:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = FALSE
+
+        bird.move()
+        draw_window(win, bird)
+
+    pg.quit()
+    quit()
+
+main()
