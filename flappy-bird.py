3cc047708f5add887e0bdca1f237f468d5a7b608
Danilo Matrangolo Marano <dpc@pop-os.localdomain>
2020-10-11 18:30:59 -0300

    Created the Pipe class with colisions

--- flappy-bird.py
+++ flappy-bird.py
@@ -86,6 +86,48 @@ class Bird:
     def get_mask(self):
         return pg.mask.form_surface(self.img)
 
+class Pipe:
+    GAP = 200
+    VEL = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.hight = 0
+
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pg.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_BOTOM = PIPE_IMG
+
+        self.passed = False
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+
+    def move(self):
+        self.x -= self.VEL
+
+    def draw(self, win):
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        top_mask = pg.mask.form_surface(self.PIPE_TOP)
+        bottom_mask = pg.mask.form_surface(self.PIPE_BOTTOM)
+
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        b_poit = bird_mask.overlap(bottom_mask, bottom_offset)
+        t_poit = bird_mask.overlap(top_mask, top_offset)
+
+        if t_point or b_point:
+            return True
+        return False
 
 
 def draw_window(win,bird):
