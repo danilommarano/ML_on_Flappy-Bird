216ece8c3f1a0f7161f27f4bb0c85fa90b413d8f
Danilo Matrangolo Marano <dpc@pop-os.localdomain>
2020-10-11 19:50:41 -0300

    Finished, without AI

--- flappy-bird.py
+++ flappy-bird.py
@@ -4,6 +4,8 @@ import pygame as pg
 import neat
 import time
 
+pg.font.init()
+
 WIDTH = 550
 HEIGHT = 800
 
@@ -14,7 +16,7 @@ BIRD_IMGS =[pg.transform.scale2x(pg.image.load(os.path.join("imgs", "bird1.png")
 PIPE_IMG = pg.transform.scale2x(pg.image.load(os.path.join("imgs", "pipe.png")))
 FLOOR_IMG = pg.transform.scale2x(pg.image.load(os.path.join("imgs", "base.png")))
 BG_IMG = pg.transform.scale2x(pg.image.load(os.path.join("imgs", "bg.png")))
-
+STAT_FONT = pg.font.SysFont("comicsans", 50)
 # ──────────────────────────── Object of the Bird ──────────────────────────── #
 class Bird:
     IMGS = BIRD_IMGS
@@ -84,11 +86,11 @@ class Bird:
         win.blit(rotated_image, new_rect.topleft)
 
     def get_mask(self):
-        return pg.mask.form_surface(self.img)
+        return pg.mask.from_surface(self.img)
 
 class Pipe:
     GAP = 200
-    VEL = 5
+    VEL = 3
 
     def __init__(self, x):
         self.x = x
@@ -97,7 +99,7 @@ class Pipe:
         self.top = 0
         self.bottom = 0
         self.PIPE_TOP = pg.transform.flip(PIPE_IMG, False, True)
-        self.PIPE_BOTOM = PIPE_IMG
+        self.PIPE_BOTTOM = PIPE_IMG
 
         self.passed = False
         self.set_height()
@@ -116,37 +118,99 @@ class Pipe:
 
     def collide(self, bird):
         bird_mask = bird.get_mask()
-        top_mask = pg.mask.form_surface(self.PIPE_TOP)
-        bottom_mask = pg.mask.form_surface(self.PIPE_BOTTOM)
+        top_mask = pg.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pg.mask.from_surface(self.PIPE_BOTTOM)
 
         top_offset = (self.x - bird.x, self.top - round(bird.y))
         bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
 
-        b_poit = bird_mask.overlap(bottom_mask, bottom_offset)
-        t_poit = bird_mask.overlap(top_mask, top_offset)
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        t_point = bird_mask.overlap(top_mask, top_offset)
 
         if t_point or b_point:
             return True
         return False
 
+class Base:
+    VEL = 3
+    WIDTH = FLOOR_IMG.get_width()
+    IMG = FLOOR_IMG
 
-def draw_window(win,bird):
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+
+def draw_window(win, bird, pipes, base, score):
     win.blit(BG_IMG, (0,0))
+
+    for pipe in pipes:
+        pipe.draw(win)
+
+    text = STAT_FONT.render("Score: " + str(score), 1, (255, 255, 255))
+    win.blit(text, (WIDTH - 10 - text.get_width(), 10 ))
+
+    base.draw(win)
+
     bird.draw(win)
     pg.display.update()
 
 def main():
-    bird = Bird(200, 200)
+    bird = Bird(230, 350)
+    base = Base(730)
+    pipes = [Pipe(700)]
     run = True
     win = pg.display.set_mode((WIDTH, HEIGHT))
+    clock = pg.time.Clock()
+
+    score = 0
 
     while run:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = FALSE
 
-        bird.move()
-        draw_window(win, bird)
+        #bird.move()
+        add_pipe = False
+        rem = []
+        for pipe in pipes:
+            if pipe.collide(bird):
+                pass
+
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+
+            pipe.move()
+
+        if add_pipe:
+            score += 1
+            pipes.append(Pipe(700))
+
+        for r in rem:
+            pipes.remove(r)
+
+        if bird.y + bird.img.get_height() >= 730:
+             pass
+        base.move()
+        draw_window(win, bird, pipes, base, score)
 
     pg.quit()
     quit()
